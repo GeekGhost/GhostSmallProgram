d84b1d2835d07f0456f2dc03c69fedfb704edd06
zjg <2231856598@qq.com>
2018-01-12 15:39:58 +0800

    增加首页导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,62 +1,117 @@
 //index.js
 //获取应用实例
-const app = getApp()
+// const app = getApp()
 
+// Page({
+//   data: {
+//     motto: 'Hello World',
+//     userInfo: {},
+//     hasUserInfo: false,
+//     canIUse: wx.canIUse('button.open-type.getUserInfo')
+//   },
+//   //事件处理函数
+//   bindViewTap: function() {
+//     wx.navigateTo({
+//       url: '../logs/logs'
+//     })
+//   },
+//   onLoad: function () {
+//     if (app.globalData.userInfo) {
+//       this.setData({
+//         userInfo: app.globalData.userInfo,
+//         hasUserInfo: true
+//       })
+//     } else if (this.data.canIUse){
+//       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+//       // 所以此处加入 callback 以防止这种情况
+//       app.userInfoReadyCallback = res => {
+//         this.setData({
+//           userInfo: res.userInfo,
+//           hasUserInfo: true
+//         })
+//       }
+//     } else {
+//       // 在没有 open-type=getUserInfo 版本的兼容处理
+//       wx.getUserInfo({
+//         success: res => {
+//           app.globalData.userInfo = res.userInfo
+//           this.setData({
+//             userInfo: res.userInfo,
+//             hasUserInfo: true
+//           })
+//         }
+//       })
+//     }
+//   },
+//   getUserInfo: function(e) {
+//     console.log(e)
+//     app.globalData.userInfo = e.detail.userInfo
+//     this.setData({
+//       userInfo: e.detail.userInfo,
+//       hasUserInfo: true
+//     })
+//   },
+ 
+// // Register a Page.
+//     changeName: function(e) {
+//       // sent data change to view
+//       this.setData({
+//         name: 'MINA'
+//       })
+//     }
+// })
+
+
+//index.js
+//获取应用实例
+var app = getApp();
+var mtabW;
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    tabs: ["精选", "专题", "发现", "我的"],
+    activeIndex: 0,
+    slideOffset: 0,
+    tabW: 0
   },
   //事件处理函数
-  bindViewTap: function() {
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        mtabW = res.windowWidth / 4; //设置tab的宽度
+        that.setData({
+          tabW: mtabW
+        })
+      }
+    });
+  },
+  //事件处理函数
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  tabClick: function (e) {
+    var that = this;
+    var idIndex = e.currentTarget.id;
+    var offsetW = e.currentTarget.offsetLeft;
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+      activeIndex: idIndex,
+      slideOffset: offsetW
+    });
   },
- 
-// Register a Page.
-    changeName: function(e) {
-      // sent data change to view
-      this.setData({
-        name: 'MINA'
-      })
+  bindChange: function (e) {
+    var current = e.detail.current;
+    if ((current + 1) % 4 == 0) {
+
     }
+    var offsetW = current * mtabW; //2种方法获取距离文档左边有多少距离
+    this.setData({
+      activeIndex: current,
+      slideOffset: offsetW
+    });
+
+  }
+
 })
+
